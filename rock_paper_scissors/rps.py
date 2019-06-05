72ddb3f37130e442cd3010fdc2cd7fceeede455a
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-06-05 17:18:46 -0400

    added rps solution

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python
 
 import sys
+import math
+
+
+def ternary(n, length):
+    nums = []
+    if n > 0:
+        while n:
+            n, r = divmod(n, 3)
+            nums.append(str(r))
+    while len(nums) < length:
+        nums.append('0')
+    return ['rock' if elem == '0' else 'paper' if elem == '1' else 'scissors' for elem in list(reversed(nums))]
+
 
 def rock_paper_scissors(n):
-  pass 
+    state = [0]*n
+    results = []
+    for x in range(3**n):
+        results.append(ternary(x, n))
+    return results
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
